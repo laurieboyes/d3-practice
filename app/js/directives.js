ff3d63102b916a40a693f614e2b799eccd040f18
Lrnk <laurie.boyes@gmail.com>
2014-08-10 21:34:34 +0100

    Bar chart tutorial part 2

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,25 +3,17 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+angular.module('myApp.directives', [])
 
-    directive('appVersion', ['version', function (version) {
+    .directive('ngChart1', [function () {
         return function (scope, elm, attrs) {
-            elm.text(version);
-        };
-    }])
-
-    .directive('chart1', [function (version) {
-        return function (scope, elm, attrs) {
-
-//            All the stuff
 
             var data = [4, 8, 15, 16, 23, 42];
 
             var scale = d3.scale.linear()
                 .domain([0, d3.max(data)])
                 .range([0, 420]);
-            
+
             d3.select("#chart1")
                 .selectAll("div")
                 .data(data)
@@ -33,6 +25,46 @@ angular.module('myApp.directives', []).
                     return d;
                 });
 
+
+        };
+    }])
+    
+    
+    .directive('ngChart2', [function () {
+        return function (scope, elm, attrs) {
+
+            var data = [4, 8, 15, 16, 23, 42];
+
+            var width = 420,
+                barHeight = 20;
+
+            var chart = d3.select("#chart2")
+                .attr("width", width)
+                .attr("height", barHeight * data.length);
+
+            var scale = d3.scale.linear()
+                .domain([0, d3.max(data)])
+                .range([0, 420]);
+
+//            So this must be a reference to some sort of collection of the bars?
+            var bar = chart.selectAll("g")
+                .data(data)
+                .enter().append("g")
+                .attr("transform", function (d, i){
+                    return "translate(0, " + i * barHeight +")";
+                });
+            
+            bar.append("rect")
+                .attr("width", scale)
+                .attr("height", barHeight - 1); // So this must be relative to the parent g?
+            
+            bar.append("text")
+                .attr("x", function(d) {
+                   return scale(d) - 3; 
+                })
+                .attr("y", barHeight / 2)
+                .attr("dy", ".35em") //todo what the hell is dy?
+                .text(function(d) {return d;});
 
         };
     }])
