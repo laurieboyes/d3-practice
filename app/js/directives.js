88983e1a91a9ee52a5ed9dfadde73d7122162c9d
Lrnk <laurie.boyes@gmail.com>
2014-08-10 19:09:01 +0100

    Tutorial: Let's make a bar chart part 1
    http://bost.ocks.org/mike/bar/

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,36 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+
+    directive('appVersion', ['version', function (version) {
+        return function (scope, elm, attrs) {
+            elm.text(version);
+        };
+    }])
+
+    .directive('chart1', [function (version) {
+        return function (scope, elm, attrs) {
+
+//            All the stuff
+
+            var data = [4, 8, 15, 16, 23, 42];
+
+            var scale = d3.scale.linear()
+                .domain([0, d3.max(data)])
+                .range([0, 420]);
+            
+            d3.select("#chart1")
+                .selectAll("div")
+                .data(data)
+                .enter().append("div")
+                .style("width", function (d) {
+                    return scale(d) + "px";
+                })
+                .text(function (d) {
+                    return d;
+                });
+
+
+        };
+    }])
+;
