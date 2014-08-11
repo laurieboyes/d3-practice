3f05990cffa0321af2a27a10258c7257e4bc0cef
Lrnk <laurie.boyes@gmail.com>
2014-08-11 18:48:34 +0100

    Added data loading to chart 2 (but for some reason it's invisible)

--- app/js/directives.js
+++ app/js/directives.js
@@ -28,42 +28,56 @@ angular.module('myApp.directives', [])
 
         };
     }])
-    
-    
+
+
     .directive('ngChart2', [function () {
         return function (scope, elm, attrs) {
-
-            var data = [4, 8, 15, 16, 23, 42];
 
             var width = 420,
                 barHeight = 20;
 
             var chart = d3.select("#chart2")
-                .attr("width", width)
-                .attr("height", barHeight * data.length);
+                .attr("width", width);
 
             var scale = d3.scale.linear()
-                .domain([0, d3.max(data)])
                 .range([0, 420]);
 
-            var bar = chart.selectAll("g")
-                .data(data)
-                .enter().append("g")
-                .attr("transform", function (d, i){
-                    return "translate(0, " + i * barHeight +")";
-                });
-            
-            bar.append("rect")
-                .attr("width", scale)
-                .attr("height", barHeight - 1);
-            
-            bar.append("text")
-                .attr("x", function(d) {
-                   return scale(d) - 3; 
-                })
-                .attr("y", barHeight / 2)
-                .attr("dy", ".35em") // this vertically aligns text
-                .text(function(d) {return d;});
+            d3.csv("data/data.csv", type, function (error, data) {
+
+                scale.domain([0, d3.max(data, function (d) {
+                    return d.value;
+                })]);
+
+                chart.attr("height", barHeight * data.length);
+
+                var bar = chart.selectAll("g")
+                    .data(data)
+                    .enter().append("g")
+                    .attr("transform", function (d, i) {
+                        return "translate(0, " + i * barHeight + ")";
+                    });
+
+                bar.append("rect")
+                    .attr("width", function (d) {
+                        scale(d.value);
+                    })
+                    .attr("height", barHeight - 1);
+
+                bar.append("text")
+                    .attr("x", function (d) {
+                        return scale(d.value) - 3;
+                    })
+                    .attr("y", barHeight / 2)
+                    .attr("dy", ".35em") // this vertically aligns text
+                    .text(function (d) {
+                        return d.value;
+                    });
+            });
+
+            function type(d) {
+                d.value = +d.value; // coerce to number
+                return d;
+            }
 
         };
     }])
