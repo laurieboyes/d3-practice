e1c6dc637361bbb1d7560a3d33f0bd7b0300a4a8
Lrnk <laurie.boyes@gmail.com>
2014-08-17 19:17:28 +0100

    Rotated barchart

--- app/js/directives.js
+++ app/js/directives.js
@@ -32,7 +32,6 @@ angular.module('myApp.directives', ['d3'])
         };
     }])
 
-
     .directive('ngChart2', ['d3Service', function (d3Service) {
         return {
             link: function (scope, elm, attrs) {
@@ -93,42 +92,47 @@ angular.module('myApp.directives', ['d3'])
             link: function (scope, elm, attrs) {
                 d3Service.d3().then(function (d3) {
 
-                    var width = 420,
-                        barHeight = 20;
+                    var height = 400,
+                        width = 600;
 
                     var chart = d3.select(elm[0])
+                        .attr("height", height)
                         .attr("width", width);
 
-                    var scale = d3.scale.linear()
-                        .range([0, 420]);
+                    var yScale = d3.scale.linear()
+                        .range([0, height]);
 
                     d3.csv("data/data.csv", type, function (error, data) {
 
-                        scale.domain([0, d3.max(data, function (d) {
+                        var barWidth = width / data.length;
+                        
+                        yScale.domain([0, d3.max(data, function (d) {
                             return d.value;
                         })]);
-
-                        chart.attr("height", barHeight * data.length);
-
+                        
                         var bar = chart.selectAll("g")
                             .data(data)
                             .enter().append("g")
                             .attr("transform", function (d, i) {
-                                return "translate(0, " + i * barHeight + ")";
+                                return "translate(" + i * barWidth + ", 0)";
                             });
 
                         bar.append("rect")
-                            .attr("width", function (d) {
-                                return scale(d.value);
+                            .attr("height", function (d) {
+                                return yScale(d.value);
                             })
-                            .attr("height", barHeight - 1);
+                            .attr("y", function(d) {
+                                return height - yScale(d.value)    
+                            }) 
+                            .attr("width", barWidth - 1);
 
                         bar.append("text")
-                            .attr("x", function (d) {
-                                return scale(d.value) - 3;
+                            .attr("x", barWidth / 2)
+                            .attr("y", function (d) {
+                                return (height - yScale(d.value)) + 3;
                             })
-                            .attr("y", barHeight / 2)
-                            .attr("dy", ".35em") // this vertically aligns text
+                            .attr("dy", ".75em")
+                            .attr("dx", ".35em")
                             .text(function (d) {
                                 return d.value;
                             });
